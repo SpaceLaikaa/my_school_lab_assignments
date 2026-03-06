e1f31667d273789f0fc50eef055e20e124622330
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-03-06 12:18:55 +0300

    Start on lab 3, add Student class with getters setters and constructor.

---
+++ lab_projects_Semester2/s2lab3/Student.java
@@ -0,0 +1,23 @@
+package lab_projects_Semester2.s2lab3;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private double gpa;
+
+    public Student(int studentID, String name, double gpa){
+        this.studentID = studentID;
+        this.name=name;
+        this.gpa=gpa;
+    }
+
+    public int getStudentID(){return studentID;}
+    public String  getName(){return name;}
+    public double getGpa(){return gpa;}
+
+    public void setStudentID(int studentID){this.studentID = studentID;}
+    public void setName(String name){this.name=name;}
+    public void setGpa(double gpa){this.gpa=gpa;}
+
+
+}
