e5d05f5364d837c527e78f3f495abf2f3cbb1e70
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-03-13 12:28:08 +0300

    Add constructor, safetyCheck method, calculateRental method to find out cost to daily rate.

---
+++ lab_projects_Semester2/s2lab4/Truck.java
@@ -0,0 +1,5 @@
+package lab_projects_Semester2.s2lab4;
+
+public class Truck extends Vehicle {
+
+}
--- lab_projects_Semester2/s2lab4/Vehicle.java
+++ lab_projects_Semester2/s2lab4/Vehicle.java
@@ -2,5 +2,22 @@ package lab_projects_Semester2.s2lab4;
 
 public class Vehicle {
     private String licensePlate;
-    private int dailyRate;
+    private double dailyRate;
+
+
+    public Vehicle(String licensePlate,double dailyRate){
+        this.licensePlate = licensePlate;
+        this.dailyRate = dailyRate;
+    }
+
+    public void calculateRental(int days){
+        double costCalculation=0;
+        costCalculation = days*dailyRate;
+        System.out.println("It will cost "+ costCalculation+" for "+days+"day");
+    }
+
+    public void safetyCheck(){
+        System.out.println("License Plate: "+licensePlate+" ,Brakes: Working, Lights: Working");
+    }
+
 }
