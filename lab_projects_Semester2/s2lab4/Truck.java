ca690e5631da33bc09191eaf2f2d9778e3b86b1b
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-03-13 12:43:45 +0300

    Implemented Truck subclass and Heavy Duty Truck subclass with their method.

--- lab_projects_Semester2/s2lab4/Truck.java
+++
@@ -1,5 +0,0 @@
-package lab_projects_Semester2.s2lab4;
-
-public class Truck extends Vehicle {
-
-}
--- lab_projects_Semester2/s2lab4/Vehicle.java
+++ lab_projects_Semester2/s2lab4/Vehicle.java
@@ -20,4 +20,40 @@ public class Vehicle {
         System.out.println("License Plate: "+licensePlate+" ,Brakes: Working, Lights: Working");
     }
 
+    public class Truck extends Vehicle {
+        private double cargoCapacity;
+
+        public Truck(String licensePlate, double dailyRate) {
+            super(licensePlate, dailyRate);
+        }
+
+        @Override
+        public void safetyCheck(){
+            if(cargoCapacity<40){
+                System.out.println("Safe to ride.");
+            }
+            else if(cargoCapacity>=40){
+                System.out.println("Truck is over the weight limit(40). Unsafe Vehicle.");
+            }
+        }
+    }
+
+    public class HeavyDutyTruck extends Truck{
+        private double axleCount=0;
+        public HeavyDutyTruck(String licensePlate, double dailyRate){
+            super(licensePlate,dailyRate);
+        }
+            @Override
+            public void calculateRental(int days){
+                double costCalculation=0;
+                int extraCost=0;
+
+                if(axleCount<2&&axleCount>0){
+
+                }
+                costCalculation = days*dailyRate;
+                System.out.println("It will cost "+ costCalculation+" for "+days+"day");
+            }
+    }
+
 }
