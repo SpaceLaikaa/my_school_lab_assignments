da8751f22cedb452cfd1ac34d280d47952f3d113
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-04-03 13:18:11 +0300

    Implemented StudentDemo main method with an ArrayList contents of 4 student object.

---
+++ lab_projects_Semester2/s2lab7/StudentDemo.java
@@ -0,0 +1,48 @@
+package lab_projects_Semester2.s2lab7;
+
+import java.util.ArrayList;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+
+        Student student1 = new Student(15,"Arda",87,"Good");
+        Student student2 = new Student(31,"Hamza",84,"Very Good");
+        Student student3 = new Student(15,"Oguz",50,"Bad");
+        Student student4 = new Student(26,"Baris",98,"Meh");
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+
+        for(Student s : students ){
+            s.displayInfo();
+
+            StudentManager studentManager = new StudentManager();
+            boolean check40 = studentManager.checkStudent(s, st ->st.getGrade() >= 30 && st.getGrade()<=40);
+            boolean check90 = studentManager.checkStudent(s, st->st.getGrade() >= 85 && st.getGrade()<=90);
+            boolean check75 = studentManager.checkStudent(s, st->st.getGrade()>75);
+
+            if(check40){
+                double newGrade40 = studentManager.applyGradeOperation(s, grade -> 40);
+                s.setGrade((int)newGrade40);
+
+                System.out.println("Note: 40");
+                s.displayInfo();
+            }
+            if(check90){
+                double newGrade90 = studentManager.applyGradeOperation(s,grade -> 90);
+                s.setGrade((int)newGrade90);
+
+                System.out.println("Note: 90");
+                s.displayInfo();
+            }
+            if(check75){
+                double newGrade75 = studentManager.applyGradeOperation(s,grade -> grade+5);
+            }
+            else{studentManager.applyGradeOperation(s,grade -> grade);}
+
+        }
+    }
+}
