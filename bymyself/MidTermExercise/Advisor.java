cbfda2c8bc09eaf71f9450cd555c20d7f10a3f5a
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-04-09 17:27:41 +0300

    feat: implement item deletion with array shifting in Advisor

--- bymyself/MidTermExercise/Advisor.java
+++ bymyself/MidTermExercise/Advisor.java
@@ -1,5 +1,7 @@
 package bymyself.MidTermExercise;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Advisor {
     private int advisorId;
     private String advisorName;
@@ -16,4 +18,17 @@ public class Advisor {
             System.out.println("Couldn't place any item.(Probably Full)");
     }
 
+    public void deleteItem(String item){
+        for(int i =0;i<officeItems.length;i++){
+            if(officeItems[i]!=null && officeItems[i].equalsIgnoreCase(item)){
+                for (int j = i;j<officeItems.length;j++){
+                    officeItems[j] = officeItems[j+1];
+                }
+                officeItems[officeItems.length - 1] = null;
+                System.out.println("Successfully deleted item.");
+                return;
+            }
+        }
+        System.out.println("Couldn't find or delete your item.");
+    }
 }
