3c9543411b8e63069d91738e03fe63da81bd97ed
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-04-10 11:36:10 +0300

    test: demonstrate Component hierarchy in FactoryDemo by printing CPU, RAM, and GPU info

--- bymyself/MidTermExercise/ComputerSystem/FactoryDemo.java
+++ bymyself/MidTermExercise/ComputerSystem/FactoryDemo.java
@@ -1,4 +1,13 @@
 package bymyself.MidTermExercise.ComputerSystem;
 
 public class FactoryDemo {
+    public static void main(String[] args) {
+        Component ram = new RAM("DDR5",30.000,5,16);
+        Component gpu = new GPU("RTX 3600",10.000,8,32);
+        Component cpu = new CPU("Intel I7",15.000,15,8);
+
+        System.out.println(ram.toString());
+        System.out.println(gpu.toString());
+        System.out.println(cpu.toString());
+    }
 }
