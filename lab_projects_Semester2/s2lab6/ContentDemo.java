e5062d96dcdac5de6db819c0061ab1f8fc99f80d
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-04-16 18:47:51 +0300

    Creat a main class to test Content subclasses with iterating through content objects

---
+++ lab_projects_Semester2/s2lab6/ContentDemo.java
@@ -0,0 +1,22 @@
+package lab_projects_Semester2.s2lab6;
+
+import java.util.ArrayList;
+
+public class ContentDemo {
+    public static void main(String[] args) {
+        ArrayList<Content> contentArrayList = new ArrayList<>();
+
+        Content movie1 = new Movie(15,"Graveyard Encounters",2,false);
+        Content series1 = new Series(25,"Game Of Thrones",1,80);
+
+        contentArrayList.add(series1); contentArrayList.add(movie1);
+
+
+        for(Content content : contentArrayList){
+            content.displayInfo();
+            System.out.println("=========");
+            System.out.println("Total Duration: "+ content.calculateTotalDuration());
+            System.out.println("Category: "+ content.getRecommendationCategory());
+        }
+    }
+}
