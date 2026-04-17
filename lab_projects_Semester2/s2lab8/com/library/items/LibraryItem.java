52d0883cef41ac70042e3153f065d3efcf052714
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-04-17 14:21:35 +0300

    add LibraryItem abstract class and basic members

---
+++ lab_projects_Semester2/s2lab8/com/library/items/LibraryItem.java
@@ -0,0 +1,24 @@
+package lab_projects_Semester2.s2lab8.com.library.items;
+
+public abstract class LibraryItem {
+    public int itemId;
+    public String title;
+
+    public LibraryItem(int itemId, String title){
+        this.itemId = itemId;
+        this.title = title;
+    }
+
+    public String getTitle() {return title;}
+    public int getItemId() {return itemId;}
+
+    public void setTitle(String title) {this.title = title;}
+    public void setItemId(int itemId) {this.itemId = itemId;}
+
+    @Override
+    public String toString() {
+        return "Library Item [ID: " + itemId + ", Title: " + title + "]";
+    }
+
+    public abstract String getItemType();
+}
