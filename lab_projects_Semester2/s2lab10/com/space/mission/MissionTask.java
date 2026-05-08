dc1c4dab8692447c63cf9cdccbd533fd2e3260cf
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-08 12:18:15 +0300

    Start on lab 10, create package system and MissionTask class

---
+++ lab_projects_Semester2/s2lab10/com/space/mission/MissionTask.java
@@ -0,0 +1,9 @@
+package lab_projects_Semester2.s2lab10.com.space.mission;
+
+public class MissionTask {
+   private String name;
+   private double requiredFuel;
+   private String requiredExperience;
+   private boolean isCompleted;
+
+}
--- lab_projects_Semester2/s2lab8/com/library/core/LibraryTest.java
+++ lab_projects_Semester2/s2lab8/com/library/core/LibraryTest.java
@@ -1,4 +1,21 @@
 package lab_projects_Semester2.s2lab8.com.library.core;
 
+import lab_projects_Semester2.s2lab8.com.library.items.Book;
+import lab_projects_Semester2.s2lab8.com.library.items.LibraryItem;
+import lab_projects_Semester2.s2lab8.com.library.items.Magazine;
+
 public class LibraryTest {
+    public static void main(String[] args) {
+        Library myLibrary = new Library("IEU");
+
+        LibraryItem book1 = new Book(15,"Animal Farm","Osman");
+        LibraryItem book2 = new Book(16,"1984","Mahmut");
+        LibraryItem magazine1 = new Magazine(15,"Animal Farm",3);
+        Library.Shelf shelfBook1 = myLibrary.new Shelf(1, book1, false);
+        Library.Shelf shelfBook2 = myLibrary.new Shelf(2,book2,false);
+        Library.Shelf shelfMagazine1 = myLibrary.new Shelf(3,magazine1,true);
+
+
+
+    }
 }
