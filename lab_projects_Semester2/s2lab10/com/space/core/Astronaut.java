bd57d5b2c94f24b30e9098bf84ac73ed63a3fcc4
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-08 12:35:42 +0300

    Create Astronaut class structre and create performTask method to check experienced or not

--- lab_projects_Semester2/s2lab10/com/space/core/Astronaut.java
+++ lab_projects_Semester2/s2lab10/com/space/core/Astronaut.java
@@ -1,4 +1,35 @@
 package lab_projects_Semester2.s2lab10.com.space.core;
 
+import lab_projects_Semester2.s2lab10.com.space.mission.MissionTask;
+
 public class Astronaut {
+    private String name;
+    private int experienceLevel;
+
+    public Astronaut(String name, int experienceLevel){
+        this.name=name;
+        this.experienceLevel=experienceLevel;
+    }
+
+    public String getName() {return name;}
+    public int getExperienceLevel() {return experienceLevel;}
+
+    public void setExperienceLevel(int experienceLevel) {this.experienceLevel = experienceLevel;}
+    public void setName(String name) {this.name = name;}
+
+    @Override
+    public String toString(){
+        return "Astronaut name: "+name+"|Experience Level: "+experienceLevel;
+    }
+
+    public void performTask(MissionTask task){
+        System.out.println("Astronaut is attempting a task");
+        if(experienceLevel>= task.getRequiredExperience()){
+            System.out.println("Astronaut successfully completed the task.");
+            experienceLevel++;
+        }
+        else{
+            throw new RuntimeException("Astronaut "+name+" does not have enough experience for this task");
+        }
+    }
 }
--- lab_projects_Semester2/s2lab10/com/space/mission/MissionTask.java
+++ lab_projects_Semester2/s2lab10/com/space/mission/MissionTask.java
@@ -3,10 +3,10 @@ package lab_projects_Semester2.s2lab10.com.space.mission;
 public class MissionTask {
    private String name;
    private double requiredFuel;
-   private String requiredExperience;
+   private int requiredExperience;
    private boolean isCompleted;
 
-   public MissionTask(String name, double requiredFuel,String requiredExperience){
+   public MissionTask(String name, double requiredFuel,int requiredExperience){
        this.name=name;
        this.requiredFuel=requiredFuel;
        this.requiredExperience=requiredExperience;
@@ -15,11 +15,11 @@ public class MissionTask {
 
     public String getName() {return name;}
     public double getRequiredFuel() {return requiredFuel;}
-    public String getRequiredExperience() {return requiredExperience;}
+    public int getRequiredExperience() {return requiredExperience;}
     public boolean getIsCompleted(){return isCompleted;}
 
     public void setName(String name) {this.name = name;}
-    public void setRequiredExperience(String requiredExperience) {this.requiredExperience = requiredExperience;}
+    public void setRequiredExperience(int requiredExperience) {this.requiredExperience = requiredExperience;}
     public void setRequiredFuel(double requiredFuel) {this.requiredFuel = requiredFuel;}
     public void setCompleted(boolean completed) {isCompleted = completed;}
 
