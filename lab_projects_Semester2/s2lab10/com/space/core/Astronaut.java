9332176586cef00fa91275f589f7c63559f71e21
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-08 12:25:17 +0300

    Create MissionTask structer and Create Astronaut class

---
+++ lab_projects_Semester2/s2lab10/com/space/core/Astronaut.java
@@ -0,0 +1,4 @@
+package lab_projects_Semester2.s2lab10.com.space.core;
+
+public class Astronaut {
+}
--- lab_projects_Semester2/s2lab10/com/space/mission/MissionTask.java
+++ lab_projects_Semester2/s2lab10/com/space/mission/MissionTask.java
@@ -6,4 +6,25 @@ public class MissionTask {
    private String requiredExperience;
    private boolean isCompleted;
 
+   public MissionTask(String name, double requiredFuel,String requiredExperience){
+       this.name=name;
+       this.requiredFuel=requiredFuel;
+       this.requiredExperience=requiredExperience;
+       isCompleted=false;
+   }
+
+    public String getName() {return name;}
+    public double getRequiredFuel() {return requiredFuel;}
+    public String getRequiredExperience() {return requiredExperience;}
+    public boolean getIsCompleted(){return isCompleted;}
+
+    public void setName(String name) {this.name = name;}
+    public void setRequiredExperience(String requiredExperience) {this.requiredExperience = requiredExperience;}
+    public void setRequiredFuel(double requiredFuel) {this.requiredFuel = requiredFuel;}
+    public void setCompleted(boolean completed) {isCompleted = completed;}
+
+    @Override
+    public String toString(){
+       return "Mission Name: "+name+"|Required Experience: "+requiredExperience+"|Required Fuel: "+requiredFuel+"|Completed: "+isCompleted;
+    }
 }
