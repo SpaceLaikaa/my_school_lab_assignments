5eac8677fcf1085a1a7f9a3f535cda7f40272028
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-15 01:45:14 +0300

    Start on BackupManager, feat: implement copyInputFile with automatic file replacement support

--- lab_projects_Semester2/s2lab11/com/labmanager/service/BackupManager.java
+++ lab_projects_Semester2/s2lab11/com/labmanager/service/BackupManager.java
@@ -1,4 +1,22 @@
 package lab_projects_Semester2.s2lab11.com.labmanager.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class BackupManager {
+    public static boolean fileExists(Path path){
+        return Files.exists(path);
+    }
+
+    public static void copyInputFile(Path sourcePath, Path destinationPath){
+        try{
+            if(Files.exists(destinationPath)){
+                Files.delete(destinationPath);
+            }
+            Files.copy(sourcePath,destinationPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
