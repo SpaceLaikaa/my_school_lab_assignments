e8f6dbacca69afce1296b2f3a684d663df233c23
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-14 14:28:54 +0300

    feat: add encapsulated StudentRecord class with Serializable implementation and transient password protection.

--- lab_projects_Semester2/s2lab11/com/labmanager/model/StudentRecord.java
+++ lab_projects_Semester2/s2lab11/com/labmanager/model/StudentRecord.java
@@ -1,4 +1,43 @@
 package lab_projects_Semester2.s2lab11.com.labmanager.model;
 
-public class StudentRecord {
+import java.io.Serializable;
+
+public class StudentRecord implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int studentID;
+    private String name;
+    private int completedLabs;
+    private double averageScore;
+    private transient String temporaryPassword ; //transient = dont Serialize this variable.
+
+    public StudentRecord(int studentID,String name, int completedLabs, double averageScore, String temporaryPassword){
+        this.studentID=studentID;
+        this.completedLabs=completedLabs;
+        this.averageScore=averageScore;
+        this.temporaryPassword=temporaryPassword;
+        this.name=name;
+    }
+
+    public String getName() {return name;}
+    public double getAverageScore() {return averageScore;}
+    public int getCompletedLabs() {return completedLabs;}
+    public int getStudentID() {return studentID;}
+    public static long getSerialVersionUID() {return serialVersionUID;}
+    public String getTemporaryPassword() {return temporaryPassword;}
+
+    public void setAverageScore(double averageScore) {this.averageScore = averageScore;}
+    public void setStudentID(int studentID) {this.studentID = studentID;}
+    public void setCompletedLabs(int completedLabs) {this.completedLabs = completedLabs;}
+    public void setTemporaryPassword(String temporaryPassword) {this.temporaryPassword = temporaryPassword;}
+    public void setName(String name) {this.name = name;}
+
+    public boolean isSuccessful(){
+        if(completedLabs >= 7 && averageScore>=60){return true;}
+        else{return false;}
+    }
+
+    public String getInfo(){
+        return "Student Name: "+name+" | Student ID: "+studentID+" | Completed Labs: "+completedLabs+
+                " | Average Score: "+averageScore+" | Temporary Password: "+temporaryPassword;
+    }
 }
