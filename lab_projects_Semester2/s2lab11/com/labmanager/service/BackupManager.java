6a8de6386b24d8a1563de5bdbf16c3a9e09ce729
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-15 12:13:33 +0300

    Feat: create getter for fileSize and log method

--- lab_projects_Semester2/s2lab11/com/labmanager/service/BackupManager.java
+++ lab_projects_Semester2/s2lab11/com/labmanager/service/BackupManager.java
@@ -3,6 +3,8 @@ package lab_projects_Semester2.s2lab11.com.labmanager.service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
 
 public class BackupManager {
     public static boolean fileExists(Path path){
@@ -17,6 +19,28 @@ public class BackupManager {
             Files.copy(sourcePath,destinationPath);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static long getFileSize(Path path){
+        try{
+            return Files.size(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void appendLog(Path logPath, String message){
+        try{
+            String logMessage = LocalDateTime.now() + " - " + message;
+
+            Files.writeString(
+                    logPath,
+                    logMessage + System.lineSeparator(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
