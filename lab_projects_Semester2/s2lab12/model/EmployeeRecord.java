ebc4f852d9ff9806881759ce91b72e420c953594
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-15 12:30:23 +0300

    Add: Create lab 12 structer and EmployeeRecord class with getters setters exc.

---
+++ lab_projects_Semester2/s2lab12/model/EmployeeRecord.java
@@ -0,0 +1,21 @@
+package lab_projects_Semester2.s2lab12.model;
+
+public class EmployeeRecord {
+    private String ID;
+    private String name;
+    private String email;
+
+    public EmployeeRecord(String ID, String name, String email){
+        this.ID=ID;
+        this.name=name;
+        this.email=email;
+    }
+
+    public String getName() {return name;}
+    public String getEmail() {return email;}
+    public String getID() {return ID;}
+
+    public void setEmail(String email) {this.email = email;}
+    public void setID(String ID) {this.ID = ID;}
+    public void setName(String name) {this.name = name;}
+}
---
+++ lab_projects_Semester2/s2lab12/service/DataManager.java
@@ -0,0 +1,14 @@
+package lab_projects_Semester2.s2lab12.service;
+
+import lab_projects_Semester2.s2lab12.model.EmployeeRecord;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class DataManager {
+
+    public ArrayList<EmployeeRecord> dataHandler(Path path){
+
+        return null; //placeholder
+    }
+}
