8e5b2ff23a45d5ec9df4cdfd14c36f7fc7714ea4
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-22 12:20:18 +0300

    Start lab 13 create abstract class with base structure

---
+++ lab_projects_Semester2/s2lab13/Delivery.java
@@ -0,0 +1,21 @@
+package lab_projects_Semester2.s2lab13;
+
+public abstract class Delivery {
+    private String orderId;
+    private double distanceKm;
+    private double baseFee;
+
+    public Delivery(String orderId, double distanceKm, double baseFee){
+        this.orderId=orderId;
+        this.distanceKm=distanceKm;
+        this.baseFee=baseFee;
+    }
+
+    public double getBaseFee() {return baseFee;}
+    public double getDistanceKm() {return distanceKm;}
+    public String getOrderId() {return orderId;}
+
+    public void setBaseFee(double baseFee) {this.baseFee = baseFee;}
+    public void setOrderId(String orderId) {this.orderId = orderId;}
+    public void setDistanceKm(double distanceKm) {this.distanceKm = distanceKm;}
+}
