1647dadd474912a0067b0ba9480123d015bab5b5
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-06-13 01:35:15 +0300

    Continue Lab 13 Task 3 create generic class with boilerplate

--- lab_projects_Semester2/s2lab13/DeliveryTask/RecordBox.java
+++ lab_projects_Semester2/s2lab13/DeliveryTask/RecordBox.java
@@ -1,5 +1,16 @@
 package lab_projects_Semester2.s2lab13.DeliveryTask;
 
 public class RecordBox<T> {
+    private T[] records;
 
+    public RecordBox(T[] records) {
+        this.records = records;
+    }
+
+    public void printAll(){
+        System.out.println("Record box: ");
+        for(T array : records){
+            System.out.println(array);
+        }
+    }
 }
